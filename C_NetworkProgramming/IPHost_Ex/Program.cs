ec07f2439d19dd88ff50f2c04dc7ffb06ddbc083
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-09-28 21:57:26 +0900

    2018.09.28 과제 : Dns 메소드 2개 사용

--- C_NetworkProgramming/IPHost_Ex/Program.cs
+++ C_NetworkProgramming/IPHost_Ex/Program.cs
@@ -7,6 +7,8 @@ namespace IPHost_Ex
     {
         static void Main(string[] args)
         {
+            // ------------------------------------------------------------------------------------
+
             string host_name = "www.google.co.kr";
             // 호스트의 도메인 네임 선언
             IPHostEntry entry = Dns.GetHostEntry(host_name);
@@ -19,10 +21,27 @@ namespace IPHost_Ex
                 // IP주소 출력
                 Console.WriteLine(iPAddress);
             }
-
             string myComputer = Dns.GetHostName();
             Console.WriteLine("컴퓨터 이름: " + myComputer);
 
+            // ---------------------------------- 수업시간에 한 내용 ------------------------------
+            Console.WriteLine("----------- 과제 -----------");
+
+            IPAddress ipaddr = IPAddress.Parse("172.217.27.67");
+
+            // GetHostByAddress
+            IPHostEntry hostEntry = Dns.GetHostByAddress(ipaddr);
+            Console.WriteLine("Host Name : "+hostEntry.HostName);
+            foreach (IPAddress ip in hostEntry.AddressList)
+                Console.WriteLine("IP Address : "+ip);
+
+            // GetHostByName
+            hostEntry = Dns.GetHostByName("www.google.co.kr");
+            Console.WriteLine("Host Name : " + hostEntry.HostName);
+            foreach (IPAddress ip in hostEntry.AddressList)
+            {
+                Console.WriteLine("IP Address : " + ip);
+            }
         }
     }
 }
