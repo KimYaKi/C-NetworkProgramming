cd0b1e08796fe25ab3cebf35c9b8ebf2db4607a8
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-10-11 10:03:40 +0900

    클라이언트 오류 수정 및 주석 추가

--- C_NetworkProgramming/ConsoleClient/Program.cs
+++ C_NetworkProgramming/ConsoleClient/Program.cs
@@ -10,11 +10,6 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-            Thread serverThread = new Thread(serverFunc);
-            serverThread.IsBackground = true;
-            serverThread.Start();
-            Thread.Sleep(500);
-
             Thread clientThread = new Thread(clientFunc);
             clientThread.IsBackground = true;
             clientThread.Start();
@@ -23,47 +18,26 @@ namespace ConsoleClient
             Console.ReadLine();
         }
 
-        private static void serverFunc(object obj)
-        {
-            using (Socket srvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-            {
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 11200);
-                try
-                {
-                    srvSocket.Bind(endPoint);
-                    srvSocket.Listen(10);
-
-                    while (true)
-                    {
-                        Socket clntSocket = srvSocket.Accept();
-                        byte[] recvBytes = new byte[1024];
-                        int nRecv = clntSocket.Receive(recvBytes);
-                        string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
-                        byte[] sendBytes = Encoding.UTF8.GetBytes("Hello: " + txt);
-                        clntSocket.Send(sendBytes);
-                        clntSocket.Close();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.StackTrace);
-                }
-            }
-        }
-
         private static void clientFunc(object obj)
         {
             Socket socket = null;
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 서버와 동일한 형태의 소켓을 생성 해야 한다.
 
             EndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 11200);
+            // Client에서 IPEndPoint에 담긴 정보는 서버의 정보이다.
             socket.Connect(serverEP);
+            // 위에서 선언 한 정보를 가지고 서버에 접속한다.
+            /* 클라이언트의 주소와 포트는 데이터를 전송 할 때 
+             * 운영체제에서 새롭게 만들어서 전송하기 때문에 선언 할 필요가 없다. */
 
             byte[] buf = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
             socket.Send(buf);
 
             byte[] recvBytes = new byte[1024];
+            // 위의 정보는 서버의 정보와 맞춰주는 것이 좋다.
             int nRecv = socket.Receive(recvBytes);
+            // 받은 데이터의 량
 
             string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
             Console.WriteLine(txt);
@@ -73,5 +47,9 @@ namespace ConsoleClient
 
             Console.WriteLine("TCP Client socket: Closed");
         }
+        /*
+         * 1. 소켓 생성 (본인의 정보는 굳이 선언하지 않는다.)
+         * 2. Connect
+         */
     }
 }
