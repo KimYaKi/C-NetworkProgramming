d35e9460020c1f11cb6aa42cd0c2368edea8630b
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-11-20 16:06:02 +0900

    SMTP예제(google의 gmail서버 사용)

---
+++ SMTP/SMTP/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SMTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(220, 67);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(456, 25);
+            this.textBoxFrom.TabIndex = 0;
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Location = new System.Drawing.Point(220, 98);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.PasswordChar = '*';
+            this.textBoxPwd.Size = new System.Drawing.Size(456, 25);
+            this.textBoxPwd.TabIndex = 1;
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(220, 129);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(456, 25);
+            this.textBoxTo.TabIndex = 2;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(220, 160);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(456, 25);
+            this.textBoxTitle.TabIndex = 3;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(220, 191);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBoxBody.Size = new System.Drawing.Size(456, 192);
+            this.textBoxBody.TabIndex = 4;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(530, 404);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(146, 31);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "메일 보내기";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14F);
+            this.label1.Location = new System.Drawing.Point(58, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "보내는 사람";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 14F);
+            this.label2.Location = new System.Drawing.Point(83, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "비밀번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 14F);
+            this.label3.Location = new System.Drawing.Point(94, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "받는사람";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 14F);
+            this.label4.Location = new System.Drawing.Point(83, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "제목";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 14F);
+            this.label5.Location = new System.Drawing.Point(83, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "내용";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 447);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.textBoxFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SMTP/SMTP/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace SMTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string mailServer = "smtp.gmail.com";
+            string fromPassword = textBoxPwd.Text;
+
+            MailAddress from = new MailAddress(textBoxFrom.Text);
+            MailAddress to = new MailAddress(textBoxTo.Text);
+
+            // 메일 서버에 특정 포트 번호로 메일 전송을 요청 하는 부분
+            SmtpClient client = new SmtpClient(mailServer, 587);
+
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            // 보내는 사람의 정보를 함께 전송한다.(from.Address, fromPassword);
+            client.Credentials = new NetworkCredential(from.Address, fromPassword);
+            client.Timeout = 20000;
+
+            MailMessage message = new MailMessage(from, to)
+            {
+                Subject = textBoxTitle.Text,
+                SubjectEncoding = System.Text.Encoding.UTF8,
+                Body = textBoxBody.Text,
+                BodyEncoding = System.Text.Encoding.UTF8
+            };
+            try
+            {
+                client.Send(message);
+                MessageBox.Show("메시지 전송 성공!!");
+                textBoxBody.Clear();
+                textBoxTitle.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                message.Dispose();
+            }
+        }
+    }
+}
