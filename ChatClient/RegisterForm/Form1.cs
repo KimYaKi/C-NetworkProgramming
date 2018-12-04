15c137c02e4fe1052e478ad5ffb8dbf3e58e0dd8
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-12-05 04:22:49 +0900

    회원가입 프로토콜 구현

--- ChatClient/RegisterForm/Form1.cs
+++ ChatClient/RegisterForm/Form1.cs
@@ -16,5 +16,18 @@ namespace RegisterForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(inputID.Text.Equals("") || inputPwd.Text.Equals("") || inputName.Text.Equals(""))
+            {
+                MessageBox.Show("빈칸이 있습니다.");
+            }
+            else
+            {
+                string data = "300/" + inputID.Text + "/" + inputPwd.Text + "/" + inputName.Text + "/";
+                MessageBox.Show(data);
+            }
+        }
     }
 }
