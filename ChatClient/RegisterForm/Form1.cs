5bb413ca3914d85d2138fa7d6b82b34c103e1b88
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-12-05 04:25:10 +0900

    AES암호화 메소드 추가

--- ChatClient/RegisterForm/Form1.cs
+++ ChatClient/RegisterForm/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RegisterForm
@@ -28,6 +24,69 @@ namespace RegisterForm
                 string data = "300/" + inputID.Text + "/" + inputPwd.Text + "/" + inputName.Text + "/";
                 MessageBox.Show(data);
             }
+        }
+
+        //-------------------------------------------------------------------------------------------------
+        // 참고 사이트 https://zoonvivor.tistory.com/18
+        //AES256 암호화
+        public string AES_encrypt(string Input, string key)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = Encoding.UTF8.GetBytes("0123456789012345");
+
+            var encrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, encrypt, CryptoStreamMode.Write))
+                {
+                    byte[] xXml = Encoding.UTF8.GetBytes(Input);
+
+                    cs.Write(xXml, 0, xXml.Length);
+                }
+
+                xBuff = ms.ToArray();
+                string recvdata = Encoding.Default.GetString(xBuff);
+                
+            }
+
+            string Output = Convert.ToBase64String(xBuff);
+            return Output;
+        }
+        //AES 256 복호화
+        public string AES_decrypt(string Input, string key)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = Encoding.UTF8.GetBytes("0123456789012345");
+
+            var decrypt = aes.CreateDecryptor();
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+
+                using (var cs = new CryptoStream(ms, decrypt, CryptoStreamMode.Write))
+                {
+                    byte[] xXml = Convert.FromBase64String(Input);
+                    string recvdata = Encoding.Default.GetString(xXml);
+                    
+                    cs.Write(xXml, 0, xXml.Length);
+                }
+
+                xBuff = ms.ToArray();
+            }
+
+            string Output = Encoding.UTF8.GetString(xBuff);
+            return Output;
         }
     }
 }
