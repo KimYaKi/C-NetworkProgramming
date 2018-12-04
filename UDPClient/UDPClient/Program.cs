55ab22cdac5eda333a5f4fb7193b80e220647c66
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-12-05 04:15:50 +0900

    UDP통신 Client 및 Server 구현

---
+++ UDPClient/UDPClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static IPAddress GetCurrentIPAddress()
+        {
+            IPAddress[] addrs = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+
+            foreach (IPAddress ipAddr in addrs)
+            {
+                if (ipAddr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ipAddr;
+                }
+            }
+
+            return null;
+        }
+
+        static void clientFunc()
+        {
+            Socket clientS = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //EndPoint serverEP = new IPEndPoint(GetCurrentIPAddress(), 10200);
+            // 서버의 주소와 서버의 포트 번호를 알고 있어야 함
+            EndPoint serverEP = new IPEndPoint(IPAddress.Parse("210.123.254.97"), 10200);
+            // 비어있는 EndPoint를 생성
+            EndPoint senderEP = new IPEndPoint(IPAddress.None, 0);
+
+            // 값을 전송하기 위해 Encoding과정 후 전송
+            byte[] buf = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+            clientS.SendTo(buf, serverEP);
+
+            byte[] recvBuf = new byte[1024];
+            // 상대방이 정해져 있지 않은 상태로 값을 전송
+            int nRecv = clientS.ReceiveFrom(recvBuf, ref senderEP);
+
+            string str = Encoding.UTF8.GetString(recvBuf, 0, nRecv);
+
+            Console.WriteLine(str);
+
+            while (true)
+            {
+                str = Console.ReadLine();
+                if (str.Equals("quit")) break;
+                buf = Encoding.UTF8.GetBytes(str);
+                clientS.SendTo(buf, serverEP);
+                nRecv = clientS.ReceiveFrom(recvBuf, ref senderEP);
+                str = Encoding.UTF8.GetString(recvBuf, 0, nRecv);
+                Console.WriteLine(str);
+            }
+
+            clientS.Close();
+        }
+        static void Main(string[] args)
+        {
+            clientFunc();
+        }
+    }
+}
---
+++ UDPClient/UDP_Comm_Console/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDP_Comm_Console
+{
+    class Program
+    {
+        static Socket serverS;
+        static void serverFunc()
+        {
+            try
+            {
+                using (serverS = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+                {
+                    IPAddress ipAddress = IPAddress.Parse("210.123.254.197");         // 0.0.0.0은 소켓을 모든 IP에 바인딩할 때 사용 
+                                                                                //IPEndPoint endPoint = new IPEndPoint(ipAddress, 10200); 
+                    IPEndPoint endPoint = new IPEndPoint(ipAddress, 10200); // 위 두 줄을 IPAddress.Any를 이용하여 한 줄로 처리. 
+
+                    serverS.Bind(endPoint);
+
+                    byte[] recvBytes = new byte[1024];
+                    // IP를 설정하지 않는 이유
+                    // UDP통신은 특정 Client에 전송하는 것이 아니기 때문에 IP설정을 하지 않는다.
+                    EndPoint clientEP = new IPEndPoint(IPAddress.None, 0);
+
+                    while (true)
+                    {
+                        // ref : 값을 자동으로 채워준다.
+                        int nRecv = serverS.ReceiveFrom(recvBytes, ref clientEP);
+                        string str = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                        Console.WriteLine("From {0} : {1}", clientEP.ToString(), str);
+                        byte[] sendBytes = Encoding.UTF8.GetBytes("Resend : " + str);
+                        serverS.SendTo(sendBytes, clientEP);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(serverFunc);
+
+            th.Start();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "QUIT")
+                {
+                    break;
+                }
+            }
+            serverS.Close();
+            th.Join();
+        }
+
+    }
+}
