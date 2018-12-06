6a7aaf5ce4469afd7c09bf31a62d1511bdd16c51
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-12-06 10:03:46 +0900

    Client에 관련된 Model생성

--- ChatClient/Server/Client.cs
+++ ChatClient/Server/Client.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class Client
+    public class Client
     {
-        private string userID { get; set; }
-        private string userPwd { get; set; }
-        private string userName { get; set; }
+        private string userID;
+        private string userPwd;
+        private string userName;
+
+        public void setUserID(string userID)
+        {
+            this.userID = userID;
+        }
+        public string getUserID()
+        {
+            return userID;
+        }
+        public void setUserPwd(string userPwd)
+        {
+            this.userPwd = userPwd;
+        }
+        public string getUserPwd()
+        {
+            return userPwd;
+        }
+        public void setUserName(string userName)
+        {
+            this.userName = userName;
+        }
+        public string getUserName()
+        {
+            return userName;
+        }
     }
 }
