8396cb30dea8b3a8df8e97da484cca639e330148
kimjiha <rlawl@DESKTOP-KOHV3DK>
2018-12-06 09:44:39 +0900

    Revert "Dictionary형태로 서버에서 Client List를 생성하는 메소드"
    This reverts commit 0ba6df91402c1fb685a567507551edcce549922e.

--- ChatClient/Server/ClientChk.cs
+++ ChatClient/Server/ClientChk.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Server
 {
-    public class ClientChk
+    class ClientChk
     {
-        public ClientChk(Dictionary<string,string> List)
-        {
-            string[] clientList = File.ReadAllLines(@"..\..\ClientText.txt");
-            foreach (string show in clientList)
-            {
-                string[] data = show.Split('/');
-                List.Add(data[0], data[1]);
-            }
-        }
+        string[] client = File.ReadAllLines(@"ClientText.txt");
+
     }
 }
